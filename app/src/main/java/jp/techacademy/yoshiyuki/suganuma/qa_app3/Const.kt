aa3028483487017d5ac47c20947be76b48cd3746
sefeeld <sefeeld@yahoo.co.jp>
2020-08-29 13:18:24 +0900

    QAアプリ

---
+++ app/src/main/java/jp/techacademy/yoshiyuki/suganuma/qa_app3/Const.kt
@@ -0,0 +1,6 @@
+package jp.techacademy.yoshiyuki.suganuma.qa_app3
+
+const val UsersPATH = "users"       // Firebaseにユーザの表示名を保存するパス
+const val ContentsPATH = "contents" // Firebaseに質問を保存するバス
+const val AnswersPATH = "answers"   // Firebaseに解答を保存するパス
+const val NameKEY = "name"          // Preferenceに表示名を保存する時のキー
---
+++ app/src/main/java/jp/techacademy/yoshiyuki/suganuma/qa_app3/MainActivity.kt
@@ -0,0 +1,52 @@
+package jp.techacademy.yoshiyuki.suganuma.qa_app3
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.design.widget.FloatingActionButton
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import android.content.Intent
+import android.support.v7.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mToolbar: Toolbar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mToolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(mToolbar)
+
+        // --- ここから ---
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener { _ ->
+            // ログイン済みのユーザーを取得する
+            val user = FirebaseAuth.getInstance().currentUser
+
+            // ログインしていなければログイン画面に遷移させる
+            if (user == null) {
+                val intent = Intent(applicationContext, LoginActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        // --- ここまで修正 ---
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
