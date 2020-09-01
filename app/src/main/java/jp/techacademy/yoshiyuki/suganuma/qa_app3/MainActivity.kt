8c52c996a2c28c13ad7dbe3d424ec2df1266a675
sefeeld <sefeeld@yahoo.co.jp>
2020-09-01 21:03:08 +0900

    レッスン８ver2

--- app/src/main/java/jp/techacademy/yoshiyuki/suganuma/qa_app3/MainActivity.kt
+++ app/src/main/java/jp/techacademy/yoshiyuki/suganuma/qa_app3/MainActivity.kt
@@ -1,18 +1,23 @@
 package jp.techacademy.yoshiyuki.suganuma.qa_app3
 
+import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import android.content.Intent
-import android.support.v7.widget.Toolbar
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var mToolbar: Toolbar
+    private var mGenre = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +25,6 @@ class MainActivity : AppCompatActivity() {
         mToolbar = findViewById(R.id.toolbar)
         setSupportActionBar(mToolbar)
 
-        // --- ここから ---
         val fab = findViewById<FloatingActionButton>(R.id.fab)
         fab.setOnClickListener { _ ->
             // ログイン済みのユーザーを取得する
@@ -32,10 +36,19 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         }
-        // --- ここまで修正 ---
+
+        // ナビゲーションドロワーの設定
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val toggle = ActionBarDrawerToggle(this, drawer, mToolbar, R.string.app_name, R.string.app_name)
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
     }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
@@ -48,5 +61,27 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        if (id == R.id.nav_hobby) {
+            mToolbar.title = "趣味"
+            mGenre = 1
+        } else if (id == R.id.nav_life) {
+            mToolbar.title = "生活"
+            mGenre = 2
+        } else if (id == R.id.nav_health) {
+            mToolbar.title = "健康"
+            mGenre = 3
+        } else if (id == R.id.nav_compter) {
+            mToolbar.title = "コンピューター"
+            mGenre = 4
+        }
+
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        drawer.closeDrawer(GravityCompat.START)
+        return true
     }
 }
